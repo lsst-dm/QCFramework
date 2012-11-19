aa6c5492b1d35093e3568a424f0f06e78f5895c2
ankitc <ankitc>
2012-11-19 18:42:29 +0000

    massive changes to wrapper. BUT not using the wrapper for now.

--- bin/qcf_wrapper.pl
+++ bin/qcf_wrapper.pl
@@ -11,18 +11,28 @@ use Data::Dumper;
 use DB::FileUtils;
 use File::Find;
 
-my ($fileList,$stdinBuffer,$desjob_dbid,$line,$infoHashref,$execDefsId,$node,$verbose,$filePath,$noingest,$dir) = ();
+my ($fileList,$stdinBuffer,$desjob_dbid,$line,$infoHashref,$wrapperInstanceId,$node,$verbose,$filePath,$noingest,$dir,$wrapperInstanceId,$execNames) = ();
 
 $verbose = 0;
 Getopt::Long::GetOptions(
     "filelist=s"    => \$fileList,
+    "execNames=s"    => \$execNames,
     "dir=s"    => \$dir,
     "noingest"    => \$noingest,
     "verbose=i"     => \$verbose,
+    "wrapperInstanceId=i"     => \$wrapperInstanceId,
 ) or usage("Invalid command line options\n");
 
-#usage("Please supply the execDefsId parameter") unless defined $execDefsId;
-usage("Please supply the filelist ") unless (defined $fileList || defined $dir);
+usage("Please supply the wrapperInstanceId") unless defined $wrapperInstanceId;
+usage("Please supply the execNames") unless defined $execNames;
+
+if(defined (<STDIN>)){
+	$mode = 'stdin';
+}else{
+	$mode = 'files';
+}
+
+#usage("Please supply the filelist ") unless (defined $fileList || defined $dir);
 
 
 
@@ -39,9 +49,7 @@ my @files;
 
         my $patternHash;
 	
-	$infoHashref->{'desjob_dbid'} = $desjob_dbid;
-	$infoHashref->{'execdefs_id'} = $execDefsId;
-	$infoHashref->{'node'} = $node;
+	$infoHashref->{'wrapperinstance_id'} = $wrapperInstanceId;
 	$infoHashref->{'verbose'} = $verbose;
 	$infoHashref->{'filepath'} = $fileList;
 
@@ -49,7 +57,7 @@ my @files;
 	### 
 	# Open the file containing a list of all the files to be read.
 	###
-	if($fileList)
+	if($mode eq 'files')
 	{
 		open (FH, "$fileList") or die "Cannot open $fileList $!";
 		my @lines=<FH>;
@@ -71,10 +79,10 @@ my @files;
 		#	print "\n the files", Dumper($resolvedFilenamesArrRef);
 			my $fileDets = extractDets(@$resolvedFilenamesArrRef,$line);
 		}
-	}
-	else
-	{
-		print "\n no filelist provided. exiting...";
+	}elsif($mode eq 'stdin'){
+			while(defined ($stdinBuffer = <STDIN>)){
+			readpipe "cat $stdinBuffer | perl qcf_controller.pl -execNames $execNames -wrapperInstanceId $wrapperInstanceId -verbose $verbose >> controllerout";
+			}
 	}
 
 sub usage {
@@ -116,172 +124,8 @@ sub extractDets {
 		$filename =  $tempHash->{'FILENAME'};
 	}
 	
-	print "\n opening file $filepath";
-	open( my $fileHandle, "<$filepath" );
-	my @lines = <$fileHandle>;
-	my @matched;
-	my $rethash;
-	my $exec;
-	foreach my $line (@lines) {
-		chomp($line);
-		#print "\n\n ----------- $line ---------";
-		###
-		# Get the name of the module from the text in the file. the pattern is beginning <name of the module>
-		###
-		@matched = ($line =~ /\-\-(\s)*(beginning)\s*(\w*)(\s*)$/);
-		if(scalar @matched > 0)
-		{
-			#print "\n matched! $3";
-			$module = $3;	
-		}
-		
-		###
-		# Get the name of the executable from the string pattern: Executing <executable name>.pl	
-		###
-		if($line =~ /Executing\s*(\/[\w|\/]*){1,}(\.pl)?(\s)?.*/){
-		$exec = $1.(defined $2 ? $2:'');	
-		#print "\n got the final $exec";
-		}
-	}
-
-	$rethash->{'module'} = $module;
-	$rethash->{'run'} = $run;
-	$rethash->{'module'} = $module;
-	print "\n the module is $module ";
-	#@matched = ($filename =~ /(.*)_($module)_(.*)/);
-	
-	###
-	# Get the job and the block names from the file	
-	###
-	my $regexCompiled =  qr/(.*)_($module)_(.*)(\.)(\w*)$/;
-	@matched = ($filename =~ $regexCompiled);
-	if(scalar @matched > 0) {
-		$desjob = $3;
-		$block = $1;
-		#print "\n MATHCED! the text from this is block $1  moduke $2 desjob $3 module otherwse $module and filename $filename",Dumper($rethash);
-	}
-	$rethash->{'desjob'} = $desjob;
-	$rethash->{'block'} = $block;
-
-	#
-	# Make a database connection
-	#
-
-	my $desdbh = DB::DESUtil->new();
-
-	# module: id, desjobid, blockid
-	# execdefs: id, moduleid, desjobid, execid	
-	# desjob: id, blockid
-	#my $dsn = "DBI:Oracle:host=141.142.226.50;sid=desoper_4";
-	#my $pw = 'ank70chips';
-	#my $user = 'ankitc';
-	#my $dsn = "DBI:Oracle:host=desdb.cosmology.illinois.edu;sid=des";
-	#my $user = 'des_admin';
-	#my $pw = 'deSadM1005';
-
-	my $row;
-	#my $desdb_dbh = DBI->connect($dsn, $user, $pw);	
-	my $sth;
-	my $sql = "select run.id, block.block_id, block.run_id from block, run where block.run_id = run.id and run.run = \'$run\' and block_name = \'$block\'";
-	print "\n the sql is $sql";
-	#$sth = $desdb_dbh->prepare($sql);
-	$sth = $desdbh->prepare($sql);
-	$sth->execute();
-	$row = $sth->fetchrow_hashref();
-	#print "\n SELECT  for $sql",Dumper($row);
-	my $block_id = $row->{'block_id'};
-	$sth->finish();
-
-	print "\n th block $block_id", Dumper($row);
-	if(not defined $block_id){
-	
-	$desdbh->disconnect();
-	die("Block ID not found.\nCheck whether the log file $logfilepath is actually registered in the new database");
-
-	}
-	
-
-	my $desjob_id = getnextId('PFW_JOB',$desdbh);
-
-	### insert the row into the temp table
-	#my $sqlInsertTempDesjob = "insert into desjob (id,name,block_id) values ($desjob_id, $desjob,$block_id)";
-	my $sqlInsertTempDesjob = " MERGE INTO PFW_JOB USING (SELECT $desjob_id ID, \'$desjob\' NAME ,$block_id block_id  FROM dual) S ON (PFW_JOB.name = S.name and PFW_JOB.block_id = S.block_id) WHEN NOT MATCHED THEN INSERT (ID, name, block_id) VALUES (S.ID, S.name, S.block_id)";
-	#print "\n the values to insert into desjob sql $sqlInsertTempDesjob : $desjob, $block_id";
-	$sth = $desdbh->prepare($sqlInsertTempDesjob);
-	$sth->execute() or print "\n err creating temp PFW_JOB table";
-	$sth->finish();
-
-	my $sqlfinalDesjobId = "select id from PFW_JOB where name = \'$desjob\' and block_id = $block_id ";
-	#print "\n sqlfinaldesjobid $sqlfinalDesjobId";
-	my $finalDesjob_id;
-	$sth = $desdbh->prepare($sqlfinalDesjobId);
-	$sth->execute() or print "\n err querying the latest exec id";
-	while(my $row_fetchDesjobId = $sth->fetchrow_hashref()){
-	
-		$finalDesjob_id = $row_fetchDesjobId->{'id'};
-	}
-
-
-	my $module_id = getnextId('pfw_module',$desdbh);
-	my $module_name;
-	### create the temp table for module to use in merge
-        #my $sqlInsertTempModule = "insert into module (id,desjob_id,block_id) values ($module_id, $desjob_id,$block_id)";
-	my $sqlInsertTempModule = " MERGE INTO pfw_module USING (SELECT $module_id ID, $finalDesjob_id PFW_JOB_id ,$block_id block_id, \'$module\' name FROM dual) S ON (pfw_module.name = S.name and pfw_module.PFW_JOB_id = S.PFW_JOB_id) WHEN NOT MATCHED THEN INSERT (ID, PFW_JOB_id, block_id,name) VALUES (S.ID, S.PFW_JOB_id, S.block_id,S.name)";
-        #print "\n the values to insert into module sql $sqlInsertTempModule : $desjob_id, $block_id";
-        $sth = $desdbh->prepare($sqlInsertTempModule);
-        $sth->execute() or print "\n err creating temp PFW_JOB table";
-	$sth->finish();
-
-
-	my $sqlfinalModuleId = "select id from pfw_module where name = \'$module\' and PFW_JOB_id = $finalDesjob_id ";
-	my $finalModule_id;
-	$sth = $desdbh->prepare($sqlfinalModuleId);
-	$sth->execute() or print "\n err querying the latest exec id";
-	while(my $row_fetchModuleId = $sth->fetchrow_hashref()){
-		$finalModule_id = $row_fetchModuleId->{'id'};
-	}
-
-
-	my $exec_id = getnextId('pfw_executable',$desdbh);
-	my $sqlMergeExec = " MERGE INTO pfw_executable USING (SELECT $exec_id ID, \'$exec\' name, \'$exec\' path FROM dual) S ON (pfw_executable.path = S.path) WHEN NOT MATCHED THEN INSERT (ID, name, path) VALUES (S.ID, S.name, S.path)";
-	#print "\n the exec merge query, $sqlMergeExec";
-	$sth = $desdbh->prepare($sqlMergeExec);
-	$sth->execute() or print "\n err querying the merge query";
-	$sth->finish();
-
-	my $sqlfinalExecId = "select id from pfw_executable where path like \'%$exec%\'";
-	$sth = $desdbh->prepare($sqlfinalExecId);
-	$sth->execute() or print "\n err querying the latest exec id";
-	while(my $row_fetchExecId = $sth->fetchrow_hashref()){
-	
-		$exec_id = $row_fetchExecId->{'id'}
-	}
-	$sth->finish();
-	print "\n ###INFO###  pfw_executable logfile: $logfilepath  executable: $exec, executable_id: $exec_id";	
-
-	my $execdefs_id = getnextId('pfw_executable_def',$desdbh);
-	my $sqlInsert = " MERGE INTO pfw_executable_def USING (SELECT $execdefs_id id,$exec_id pfw_executable_id , $finalModule_id pfw_module_id, $finalDesjob_id PFW_JOB_id FROM dual) S ON (pfw_executable_def.pfw_executable_id = S.pfw_executable_id and pfw_executable_def.pfw_module_id = S.pfw_module_id and pfw_executable_def.PFW_JOB_id = S.PFW_JOB_id) WHEN NOT MATCHED THEN INSERT (ID, pfw_executable_id, pfw_module_id,  PFW_JOB_id) VALUES (S.ID, S.pfw_executable_id, S.pfw_module_id, S.PFW_JOB_id)";
-	#my $sqlInsert = "insert into execdefs (id,exec_id,module_id, desjob_dbid) values ($execdefs_id,$exec_id, $module_id, $block_id)";
-	#print "\n the values to insert into  execdefs sql $sqlInsert: $exec_id, $module_id, $block_id, $execdefs_id";	
-	$sth = $desdbh->prepare($sqlInsert);
-	$sth->execute() or print "\n err inserting into execdefs";
-	$sth->finish();
-
-	my $sqlfinalExecDefsId = "select id from pfw_executable_def where PFW_JOB_id = $finalDesjob_id and pfw_executable_id = $exec_id and pfw_module_id = $finalModule_id ";
-	$sth = $desdbh->prepare($sqlfinalExecDefsId);
-	$sth->execute() or print "\n err querying the latest exec id";
-	my $finalExecDefs_id;	
-	while(my $row_fetchExecDefsId = $sth->fetchrow_hashref()){
-	
-		$finalExecDefs_id = $row_fetchExecDefsId->{'id'};
-	}
-
-	$desdbh->commit();
-	$desdbh->disconnect();
-
-	print "\n the final execDefsId of filepath $filepath  for $sqlfinalExecDefsId is $finalExecDefs_id";
 	if (!$noingest){
-		readpipe "cat $filepath | perl qcf_controller.pl -execDefsId $finalExecDefs_id -verbose 1 >> controllerout";
+		readpipe "cat $filepath | perl qcf_controller.pl -wrapperInstanceId $finalExecDefs_id -verbose $verbose >> controllerout";
 	}
 	else{
 		print "\n #### NOT INGESTING ####";
